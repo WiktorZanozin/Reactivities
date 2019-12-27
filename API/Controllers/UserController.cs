fa26548ae4cd3e2f04fe7053e224315da7f8ea38
Viktor <wiktor.zanozin@gmail.com>
2019-12-27 16:34:03 +0500

    End of section 12

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Application.User;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Application.Errors;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{
+    [AllowAnonymous]
+    public class UserController:BaseController
+    {
+        [HttpPost("login")]
+         public async Task<ActionResult<User>> Login(Login.Query query)
+         {
+           return await Mediator.Send(query);
+         }
+
+         [HttpPost("register")]
+         public async Task<ActionResult<User>> Register(Register.Command command)
+         {
+           return await Mediator.Send(command);
+         }
+
+         [HttpPost("register")]
+         public async Task<ActionResult<User>> CurrentUser()
+         {
+           return await Mediator.Send(new CurrentUser.Query());
+         }
+        
+    }
+}
